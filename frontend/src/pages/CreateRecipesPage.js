6275ccaac42fd890146e039305a048f0e631d116
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-04-11 11:14:56 -0400

    Create CreateRecipesPage.js

---
+++ frontend/src/pages/CreateRecipesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CreateNavBar from '../components/CreateNavBar.js';
+import CreateBody from '../components/CreateBody.js';
+
+const CreateRecipesPage = () =>
+{
+    return(
+        <div>
+            <CreateNavBar />
+            <CreateBody />
+        </div>
+    );
+}
+export default CreateRecipesPage;
