1ba828274743c9400a1eb988fba86ff42297ec32
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2023-04-11 11:14:01 -0400

    Create ViewProfilePage.js

---
+++ frontend/src/pages/ViewProfilePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProfileNavBar from '../components/ProfileNavBar.js';
+import ProfileBody from '../components/ProfileBody.js';
+
+const ProfilePage = () =>
+{
+    return(
+        <div>
+            <ProfileNavBar />.
+            <ProfileBody />
+        </div>
+    );
+}
+export default ProfilePage;
